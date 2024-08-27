fcb310d96d0563dba702d4f95bd4746a960e18ea
Max Ivanov <imaks1464@gmail.com>
2024-08-27 20:37:18 +0300

    add searching about time_update to comments

--- idea/urls.py
+++ idea/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('search/', views.search_ideas, name='search_results'),
     path('idea_detail/<int:idea_id>/', views.IdeaDetail.as_view(), name='idea'),
     path('idea_detail/<int:idea_id>/comment/', views.AddComment .as_view(), name='add_comment'),
     path('profile/<slug:username>/', views.profile, name='profile'),
@@ -15,4 +16,5 @@ urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('logout/', views.logout_user, name='logout'),
+    path('about/', views.about, name='about'),
 ]
